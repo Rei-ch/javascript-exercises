2140e772b9eaeb162690b0205d7e24511a93c872
Reichelle R <rmnrobianes@gmail.com>
2022-09-14 20:07:02 -0700

    Finish exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(itemArray, ...args) {
+    const items = args;
+    const len = items.length;
+    if (len == 0)
+        return itemArray;
+    for (let i = 0; i < len; ++i){
+        let ind = itemArray.indexOf(items[i]);
+        if (ind == -1)
+            continue;
+        else {
+            itemArray.splice(ind, 1);
+        }
+    }
+    return itemArray;
 };
 
 // Do not edit below this line
