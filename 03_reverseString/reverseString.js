4beb76affb1c84cfc982b612114dabf83888782a
Reichelle R <rmnrobianes@gmail.com>
2022-09-14 19:50:35 -0700

    Finish exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    if (string === "")
+        return ""
+
+    let stringLen = string.length-1;
+    let reversed = "";
+    while (stringLen){
+        reversed += string[stringLen];
+        --stringLen;
+    }
+    reversed += string[0];
+    return reversed;
 
 };
 
